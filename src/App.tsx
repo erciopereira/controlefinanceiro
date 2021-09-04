626c5ddc312c133cee33a43968533b722f981f83
Ercio Pereira <erciop@gmail.com>
2021-09-04 11:08:01 -0300

    Configuração de estilos globais / importação de icones / configurações gerais

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { GlobalStyle } from "./styles/global";
+
 export function App() {
   return (
     <div className="App">
-      
+      <div>Hello world</div>
+      <GlobalStyle />
     </div>
   );
 }
